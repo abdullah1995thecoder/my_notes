43b1500d075d6b2ae16fb4055d19d1796c7ac4ba
abdullah1995thecoder <abdullah077149@gmail.com>
2022-08-19 12:10:27 +0300

    v1

---
+++ lib/Screens/home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:my_notes/db/SqlDb.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  SqlDb sqlDb = SqlDb();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: () async {
+                  int response = await sqlDb.insertData(
+                      "INSERT INTO 'notes' ('note') VALUES ('Note One')");
+                  print(response);
+                },
+                child: Text("Insert Data"),
+              ),
+            ),
+            Center(
+              child: MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: () async {
+                  List<Map> response =
+                      await sqlDb.readData("SELECT * FROM 'notes'");
+                  print(response);
+                },
+                child: Text("Read Data"),
+              ),
+            ),
+            Center(
+              child: MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: () async {
+                  int response =
+                      await sqlDb.deleteData("DElETE FROM 'notes' WHERE id =7");
+                  print('$response');
+                },
+                child: Text("Delete Data"),
+              ),
+            ),
+            Center(
+              child: MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: () async {
+                  int response = await sqlDb.updateData(
+                      "UPDATE 'notes' SET 'note' = 'note nine' WHERE id = 9");
+                  print('$response');
+                },
+                child: Text("Update Data"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
